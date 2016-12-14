39d0ec5740841983f14d91ae02cce0e9d5b42507
Bruce Wayne <admin@bige0.com>
2016-12-14 13:41:21 +0800

    Update Union Find Sets.cpp

--- 4.Data-Structure/4.2-Union-Find-Sets/Union Find Sets.cpp
+++ 4.Data-Structure/4.2-Union-Find-Sets/Union Find Sets.cpp
@@ -1,18 +1,3 @@
-/*
-并查集
-结构体：DisjoinSet
-成员变量：
-    vector<int> father //元素的父亲节点，树根元素的父亲为自身
-    vector<int> rank   //树根元素所代表集合的rank
-    _max               //最大集合的元素个数
-成员函数：
-    DisjoinSet(int n);       //初始化，n个元素，处于单独集合
-    复杂度：O(n)
-    int find(int v);         //查找v所在集合的代表元
-    复杂度：均摊O(1)
-    void merge(int x,int y); //合并x所在的集合与y所在集合
-    复杂度：均摊O(1)
-*/
 struct DisjoinSet {
 	std::vector<int> father, rank,max;
 	int _max = 1;
