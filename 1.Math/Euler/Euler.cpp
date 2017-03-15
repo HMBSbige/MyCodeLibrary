f4eadeab6f4b1c3918d231064779acc0fec53edc
Bruce Wayne <admin@bige0.com>
2017-03-15 21:15:37 +0800

    欧拉函数

---
+++ 1.Math/Euler/Euler.cpp
@@ -0,0 +1,29 @@
+#include "Euler.h"
+
+ull Euler::_Euler(ull n)
+{
+	ull a = n;
+	for (int i = 2; i*i <= a; i++) {
+		if (a%i == 0) {
+			n /= i*(i - 1);
+			while (a%i == 0) a /= i;
+		}
+	}
+	if (a>1) n /= a*(a - 1);
+	return n;
+}
+ull* Euler::MakeEuler(ull N)
+{
+	ull *phi=new ull[N+1];
+	int i, j;
+	for (i = 1; i <= N; i++)
+		phi[i] = i;
+	for (i = 2; i <= N; i += 2)
+		phi[i] /= 2;
+	for (i = 3; i <= N; i += 2)
+		if (phi[i] == i) {
+			for (j = i; j <= N; j += i)
+				phi[j] = phi[j] / i * (i - 1);
+		}
+	return phi;
+}
---
+++ 1.Math/Euler/Euler.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <new>
+using namespace std;
+typedef uintmax_t ull;
+
+class Euler {
+private:
+
+public:
+	ull _Euler(ull n);
+	ull* MakeEuler(ull N);
+};
