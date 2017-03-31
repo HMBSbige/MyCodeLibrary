f59d5f311e0884cbadcec9a5f8857cedbc832fee
Bruce Wayne <admin@bige0.com>
2017-04-01 02:06:00 +0800

    DEBUG和输出文本隔离

--- Other/usual.cpp
+++ Other/usual.cpp
@@ -1,24 +1,27 @@
 #include <iostream> 
 #define DEBUG
-#ifdef DEBUG
+//#define ofile
+#ifdef ofile
 #include <fstream>	
 std::ofstream outfile;
-#endif
+#endif // ofile
 //////////////////////////////////////////////////////////////////////
 int main(int argc, char *argv[])
 {
 	std::ios::sync_with_stdio(false);
 	setlocale(LC_ALL, "");
-#ifdef DEBUG	
+#ifdef ofile	
 	outfile.open("C:\\Users\\Bruce Wayne\\Desktop\\out.txt", std::ios::trunc);
 	std::cout.rdbuf(outfile.rdbuf());
 #endif
 //////////////////////////////////////////////////////////////////////
 
 //////////////////////////////////////////////////////////////////////
-#ifdef DEBUG
+#ifdef ofile
 	outfile.close();
+#endif
+#ifdef DEBUG
 	system("pause");
-#endif // DEBUG
+#endif
 	return 0;
 }
