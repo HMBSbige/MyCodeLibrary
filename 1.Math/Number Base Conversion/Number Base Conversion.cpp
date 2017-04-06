11d1bdae0442d0d26ba0846bf00235cb751941be
Bruce Wayne <admin@bige0.com>
2017-04-07 01:18:51 +0800

    修复十进制转换判断条件

--- 1.Math/Number Base Conversion/Number Base Conversion.cpp
+++ 1.Math/Number Base Conversion/Number Base Conversion.cpp
@@ -6,22 +6,25 @@ T StringToNum(const std::string& s)//字符串转数字
 	ss >> num;
 	return num;
 }
-std::string NumberBaseConversion(const uint8_t& x, const uint8_t& y,const std::string& s) //2<=x,y<=36
+std::string NumberBaseConversion(const uint8_t& x, const uint8_t& y, const std::string& s) //2<=x,y<=36
 {
 	std::string res;
 	size_t sum = 0;
-	if(x==10)
-		sum = StringToNum<size_t>(s);
+	if (x == 10)
+		if(s[0] == '-')
+			sum = StringToNum<size_t>(s.substr(1));
+		else
+			sum = StringToNum<size_t>(s);
 	else
 		for (int i = 0; i < s.size(); ++i) {
 			if (s[i] >= '0' && s[i] <= '9')
 				sum = sum*x + s[i] - '0';
-			else if(s[i] >= 'A' && s[i] <= 'Z')
+			else if (s[i] >= 'A' && s[i] <= 'Z')
 				sum = sum*x + s[i] - 'A' + 10;
 			else if (s[i] >= 'a' && s[i] <= 'z')
 				sum = sum*x + s[i] - 'a' + 10;
 		}
-	while (sum){
+	while (sum) {
 		char tmp = sum%y;
 		sum /= y;
 		if (tmp <= 9)
