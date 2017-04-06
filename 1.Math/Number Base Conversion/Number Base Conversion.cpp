acc886ae41cec9ed4adc6e7343860d2161c3f64f
Bruce Wayne <admin@bige0.com>
2017-04-06 14:34:27 +0800

    进制转换

--- 1.Math/Number Base Conversion/Number Base Conversion.cpp
+++ 1.Math/Number Base Conversion/Number Base Conversion.cpp
@@ -1,15 +1,26 @@
+template<class T>
+T StringToNum(const std::string& s)//字符串转数字
+{
+	T num;
+	std::stringstream ss(s);
+	ss >> num;
+	return num;
+}
 std::string NumberBaseConversion(const uint8_t& x, const uint8_t& y,const std::string& s) //2<=x,y<=36
 {
 	std::string res;
-	int sum = 0;
-	for (int i = 0; i < s.size(); ++i) {
-		if (s[i] >= '0' && s[i] <= '9')
-			sum = sum*x + s[i] - '0';
-		else if(s[i] >= 'A' && s[i] <= 'Z')
-			sum = sum*x + s[i] - 'A' + 10;
-		else if (s[i] >= 'a' && s[i] <= 'z')
-			sum = sum*x + s[i] - 'a' + 10;
-	}
+	size_t sum = 0;
+	if(x==10)
+		sum = StringToNum<size_t>(s);
+	else
+		for (int i = 0; i < s.size(); ++i) {
+			if (s[i] >= '0' && s[i] <= '9')
+				sum = sum*x + s[i] - '0';
+			else if(s[i] >= 'A' && s[i] <= 'Z')
+				sum = sum*x + s[i] - 'A' + 10;
+			else if (s[i] >= 'a' && s[i] <= 'z')
+				sum = sum*x + s[i] - 'a' + 10;
+		}
 	while (sum){
 		char tmp = sum%y;
 		sum /= y;
