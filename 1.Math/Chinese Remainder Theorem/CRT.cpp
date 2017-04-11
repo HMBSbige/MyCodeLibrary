6ef5480074444ca157dc982d370ecdcd5037da1b
Bruce Wayne <admin@bige0.com>
2017-04-11 14:52:42 +0800

    another mul_inv &  modulus are not relatively-prime

--- 1.Math/Chinese Remainder Theorem/CRT.cpp
+++ 1.Math/Chinese Remainder Theorem/CRT.cpp
@@ -1,5 +1,17 @@
 typedef uint64_t ull;
 typedef int64_t ll;
+void ex_gcd(ll a, ll b, ll &x, ll &y, ll &d){
+    if (!b) {d = a, x = 1, y = 0;}
+    else{
+        ex_gcd(b, a % b, y, x, d);
+        y -= x * (a / b);
+    }
+}
+ll mul_inv(ll t, ll p){//如果不存在，返回-1 
+    ll d, x, y;
+    ex_gcd(t, p, x, y, d);
+    return d == 1 ? (x % p + p) % p : -1;
+}
 inline ll mul_inv(ll a, ll b)// returns x where (a * x) % b == 1
 {
 	ll b0 = b, t, q;
@@ -14,7 +26,7 @@ inline ll mul_inv(ll a, ll b)// returns x where (a * x) % b == 1
 	return x1;
 }
 ull chinese_remainder(std::vector<ull> n, std::vector<ull> a)//x=a[i](mod n[i])
-{
+{//n are relatively-prime
 	ull p, prod = 1, sum = 0;
 	register size_t i;
 	for (i = 0; i < n.size(); ++i) prod *= n[i];
@@ -25,4 +37,20 @@ ull chinese_remainder(std::vector<ull> n, std::vector<ull> a)//x=a[i](mod n[i])
 		sum %= prod;
 	}
 	return (sum+ prod) % prod;
+}
+typedef std::pair<ll, ll> PLL;
+PLL linear(std::vector<ll> A, std::vector<ll> B, std::vector<ll> M) //return x where A[i]*x = B[i] (mod M[i]) 
+{
+	ll x = 0, m = 1;
+	register size_t i;
+	for (i = 0; i < A.size(); i++) {
+		ll a = A[i] * m, b = B[i] - A[i] * x, d = gcd(M[i], a);
+		if (b % d != 0)  
+			return PLL(0, -1);//if there are no possible values
+		ll t = b / d * mul_inv(a / d, M[i] / d) % (M[i] / d);
+		x = x + m*t;
+		m *= M[i] / d;
+	}
+	x = (x % m + m) % m;
+	return PLL(x, m);//m is lcm
 }
