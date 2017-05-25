46279554d5166eb43b87721300bd5df62ba20294
Bruce Wayne <admin@bige0.com>
2017-05-25 11:30:48 +0800

    修复空格，warning

--- Other/Character Encoding Conversion/character_encoding_conversion.cpp
+++ Other/Character Encoding Conversion/character_encoding_conversion.cpp
@@ -1,73 +1,69 @@
-#include "character_encoding_conversion.h"  
-CharacterEncodingConversion::CharacterEncodingConversion()  
-:m_char(NULL)  
-,m_wchar(NULL)  
-{  
-}  
-CharacterEncodingConversion::~CharacterEncodingConversion()  
-{  
-    Release();  
-}  
-char* CharacterEncodingConversion::WcharToChar(const wchar_t* wp)  
-{  
-    Release();  
-    int len= WideCharToMultiByte(CP_ACP,0,wp,wcslen(wp),NULL,0,NULL,NULL);  
-    m_char=new char[len+1];  
-    WideCharToMultiByte(CP_ACP,0,wp,wcslen(wp),m_char,len,NULL,NULL); 
-    m_char[len]='\0';  
-    return m_char;  
-}  
-wchar_t* CharacterEncodingConversion::CharToWchar(const char* c)  
-{  
-    Release();  
-    int len = MultiByteToWideChar(CP_ACP,0,c,strlen(c),NULL,0);  
-    m_wchar=new wchar_t[len+1];  
-    MultiByteToWideChar(CP_ACP,0,c,strlen(c),m_wchar,len);  
-    m_wchar[len]='\0';  
-    return m_wchar;  
-}  
-void CharacterEncodingConversion::Release()  
-{  
-    if(m_char)  
-    {  
-        delete m_char;  
-        m_char=NULL;  
-    }  
-    if(m_wchar)  
-    {  
-        delete m_wchar;  
-        m_wchar=NULL;  
-    }  
-}  
-char* CharacterEncodingConversion::StringToChar(const string& s)  
-{  
-    return const_cast<char*>(s.c_str());  
-}  
-char* CharacterEncodingConversion::WstringToChar(const std::wstring &ws)  
-{  
-    const wchar_t* wp=ws.c_str();  
-    return WcharToChar(wp);  
-}  
-wchar_t* CharacterEncodingConversion::WstringToWchar(const std::wstring &ws)  
-{  
-    return const_cast<wchar_t*>(ws.c_str());  
-}  
-wchar_t* CharacterEncodingConversion::StringToWchar(const string& s)  
-{  
-    const char* p=s.c_str();  
-    return CharToWchar(p);
-}  
-string CharacterEncodingConversion::WstringToString(const std::wstring &ws)  
-{  
-    string s;  
-    char* p=WstringToChar(ws);  
-    s.append(p);  
-    return s;  
-}  
-wstring CharacterEncodingConversion::StringToWstring(const std::string &s)  
-{  
-    wstring ws;  
-    wchar_t* wp=StringToWchar(s);  
-    ws.append(wp);  
-    return ws;  
+#include "character_encoding_conversion.h"
+CharacterEncodingConversion::CharacterEncodingConversion()
+:m_char(nullptr)
+,m_wchar(nullptr)
+{}
+CharacterEncodingConversion::~CharacterEncodingConversion(){Release();}
+char* CharacterEncodingConversion::WcharToChar(const wchar_t* wp)
+{
+	Release();
+	auto len= WideCharToMultiByte(CP_ACP,0,wp,int(wcslen(wp)),nullptr,0,nullptr,nullptr);
+	m_char=new char[len+1];
+	WideCharToMultiByte(CP_ACP,0,wp,int(wcslen(wp)),m_char,len,nullptr,nullptr);
+	m_char[len]='\0';
+	return m_char;
+}
+wchar_t* CharacterEncodingConversion::CharToWchar(const char* c)
+{
+	Release();
+	auto len = MultiByteToWideChar(CP_ACP,0,c,int(strlen(c)),nullptr,0);
+	m_wchar=new wchar_t[len+1];
+	MultiByteToWideChar(CP_ACP,0,c,int(strlen(c)),m_wchar,len);
+	m_wchar[len]='\0';
+	return m_wchar;
+}
+void CharacterEncodingConversion::Release()
+{
+	if(m_char)
+	{
+		delete m_char;
+		m_char= nullptr;
+	}
+	if(m_wchar)
+	{
+		delete m_wchar;
+		m_wchar= nullptr;
+	}
+}
+char* CharacterEncodingConversion::StringToChar(const string& s)
+{
+	return const_cast<char*>(s.c_str());
+}
+char* CharacterEncodingConversion::WstringToChar(const std::wstring &ws)
+{
+	const wchar_t* wp=ws.c_str();
+	return WcharToChar(wp);
+}
+wchar_t* CharacterEncodingConversion::WstringToWchar(const std::wstring &ws)
+{
+	return const_cast<wchar_t*>(ws.c_str());
+}
+wchar_t* CharacterEncodingConversion::StringToWchar(const string& s)
+{
+	const char* p=s.c_str();
+	return CharToWchar(p);
+}
+string CharacterEncodingConversion::WstringToString(const std::wstring &ws)
+{
+	string s;
+	char* p=WstringToChar(ws);
+	s.append(p);
+	return s;
+}
+wstring CharacterEncodingConversion::StringToWstring(const std::string &s)
+{
+	wstring ws;
+	wchar_t* wp=StringToWchar(s);
+	ws.append(wp);
+	return ws;
 }
--- Other/Character Encoding Conversion/character_encoding_conversion.h
+++ Other/Character Encoding Conversion/character_encoding_conversion.h
@@ -1,23 +1,22 @@
 #pragma once
-#include <iostream>
-#include <windows.h>
+#include <Windows.h>
 #include <string>
 using namespace std;
-class CharacterEncodingConversion  
-{  
-public:  
-    CharacterEncodingConversion();  
-    virtual~ CharacterEncodingConversion();  
-    char* WcharToChar(const wchar_t* wp);  
-    char* StringToChar(const string& s);  
-    char* WstringToChar(const wstring& ws);  
-    wchar_t* CharToWchar(const char* c);  
-    wchar_t* WstringToWchar(const wstring& ws);  
-    wchar_t* StringToWchar(const string& s);  
-    wstring StringToWstring(const string& s);  
-    string WstringToString(const wstring& ws);  
-    void Release();  
-private:  
-    char* m_char;  
-    wchar_t* m_wchar;  
-}; 
+class CharacterEncodingConversion
+{
+public:
+	CharacterEncodingConversion();
+	virtual~ CharacterEncodingConversion();
+	char* WcharToChar(const wchar_t* wp);
+	static char* StringToChar(const string& s);
+	char* WstringToChar(const wstring& ws);
+	wchar_t* CharToWchar(const char* c);
+	static wchar_t* WstringToWchar(const wstring& ws);
+	wchar_t* StringToWchar(const string& s);
+	wstring StringToWstring(const string& s);
+	string WstringToString(const wstring& ws);
+	void Release();
+private:
+	char* m_char;
+	wchar_t* m_wchar;
+};
