830567da7fc124cced878fb38596251035dde327
HMBSbige <admin@bige0.com>
2017-12-30 20:58:15 +0800

    (C#)Get the MatchID from a CS:GO match share link.

---
+++ Misc/ShareLinkToMatchID.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Linq;
+
+namespace CSGO
+{
+    internal static class ShareLink
+    {
+        public static ulong ToMatchID(string sharecode)
+        {
+            sharecode = sharecode.Replace(@"CSGO", @"").Replace(@"-", @"");
+            sharecode = new string(sharecode.ToCharArray().Reverse().ToArray());
+            const string dictionary = @"ABCDEFGHJKLMNOPQRSTUVWXYZabcdefhijkmnopqrstuvwxyz23456789";
+            byte[] result = new byte[18];
+            foreach (var cur_char in sharecode)
+            {
+                byte[] tmp = new byte[18];
+                int addval = dictionary.IndexOf(cur_char.ToString(), StringComparison.Ordinal);
+                int carry;
+                int v;
+
+                for (int t = 17; t >= 0; t--)
+                {
+                    carry = 0;
+                    for (int s = t; s >= 0; s--)
+                    {
+                        if (t - s == 0)
+                        {
+                            v = tmp[s] + result[t] * 57;
+                        }
+                        else
+                        {
+                            v = 0;
+                        }
+                        v = v + carry;
+                        carry = v >> 8;
+                        tmp[s] = (byte)(v & 0xFF);
+                    }
+                }
+
+                result = tmp;
+                carry = 0;
+
+                for (int t = 17; t >= 0; t--)
+                {
+                    if (t == 17)
+                    {
+                        v = result[t] + addval;
+                    }
+                    else
+                    {
+                        v = result[t];
+                    }
+                    v = v + carry;
+                    carry = v >> 8;
+                    result[t] = (byte)(v & 0xFF);
+                }
+            }
+            var matchid = BitConverter.ToUInt64(result, 0);
+
+            return matchid;
+        }
+    }
+}
