f5dd4dd130e465ecd5a0fb548a1762ff26972766
HMBSbige <admin@bige0.com>
2017-12-31 17:14:22 +0800

    Windows NT 4.0以上的繁简转换

---
+++ Misc/ChineseStringUtility.cs
@@ -0,0 +1,28 @@
+﻿using System.Runtime.InteropServices;
+
+namespace SC_TC_Conversion
+{
+    public static class ChineseStringUtility
+    {
+        private const int LOCALE_SYSTEM_DEFAULT = 0x0800;
+        private const int LCMAP_SIMPLIFIED_CHINESE = 0x02000000;
+        private const int LCMAP_TRADITIONAL_CHINESE = 0x04000000;
+
+        [DllImport(@"kernel32", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern int LCMapString(int Locale, int dwMapFlags, string lpSrcStr, int cchSrc, [Out] string lpDestStr, int cchDest);
+
+        public static string ToSimplified(string source)
+        {
+            var target = new string(' ', source.Length);
+            int ret = LCMapString(LOCALE_SYSTEM_DEFAULT, LCMAP_SIMPLIFIED_CHINESE, source, source.Length, target, source.Length);
+            return target;
+        }
+
+        public static string ToTraditional(string source)
+        {
+            var target = new string(' ', source.Length);
+            int ret = LCMapString(LOCALE_SYSTEM_DEFAULT, LCMAP_TRADITIONAL_CHINESE, source, source.Length, target, source.Length);
+            return target;
+        }
+    }
+}
